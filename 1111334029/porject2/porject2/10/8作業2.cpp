097b1cb09aadae6c0d545bf13c499c974f19c982
Hilibrary <97895846+Hilibr@users.noreply.github.com>
2025-10-08 16:31:33 +0800

    10/8作業
    10/8作業

---
+++ 1111334029/porject2/porject2/10/8作業2.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(void)
+{
+	double high = 0;
+	double kg = 0;
+	double BMI = 0;
+	printf("請輸入身高:\n");
+	scanf_s("%d", &high);
+	printf("請輸入體重:\n");
+	scanf_s("%d", &kg);
+}
